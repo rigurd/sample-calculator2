a6185ffe1c9992c61e92cba417225c8722f7f0b7
Rickard Berglund <rigurd@hotmail.com>
2016-10-07 14:15:18 +0200

    my second commit uehuehuehueheuehu

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -22,17 +22,103 @@ public class CalculatorTest {
 		double result = 0.0;
 		
 		for (int i = 0; i < 5; i++){
-			
 			first = rand1.nextDouble()*10;
-			second =rand1.nextDouble()*10;
-			
-			
+			second = rand1.nextDouble()*10;
 			result = first + second;
-			
-			LOG.info("Testing the method add with" +first+" and "+second);
+			LOG.info("Testing the method add with " +first+" and "+second);
 			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
 		}
 	}
-
+	@Test
+	public void test2() {
+		
+		double first = 0.0;
+		double second = 0.0;
+		double result = 0.0;
+		
+		for (int i = 0; i < 5; i++){
+			first = rand1.nextDouble()*100;
+			second = rand1.nextDouble()*100;
+			result = first + second;
+			LOG.info("Testing the method add with " +first+" and "+second);
+			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
+		}
+	}
+	@Test
+	public void test3() {
+		
+		double first = 0.0;
+		double second = 0.0;
+		double result = 0.0;
+		
+		for (int i = 0; i < 5; i++){
+			first = rand1.nextDouble()*1000;
+			second = rand1.nextDouble()*1000;
+			result = first + second;
+			LOG.info("Testing the method add with " +first+" and "+second);
+			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
+		}
+	}
+	@Test
+	public void test4() {
+		
+		double first = 0.0;
+		double second = 0.0;
+		double result = 0.0;
+		
+		for (int i = 0; i < 5; i++){
+			first = rand1.nextDouble()*0;
+			second = rand1.nextDouble()*0;
+			result = first + second;
+			LOG.info("Testing the method add with " +first+" and "+second);
+			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
+		}
+	}
+	
+	@Test
+	public void test5() {
+		
+		double first = 0.0;
+		double second = 0.0;
+		double result = 0.0;
+		
+		for (int i = 0; i < 5; i++){
+			first = rand1.nextDouble()*-10;
+			second = rand1.nextDouble()*-10;
+			result = first + second;
+			LOG.info("Testing the method add with " +first+" and "+second);
+			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
+		}
+	}
+	@Test
+	public void test6() {
+		
+		double first = 0.0;
+		double second = 0.0;
+		double result = 0.0;
+		
+		for (int i = 0; i < 5; i++){
+			first = rand1.nextDouble()*-100;
+			second = rand1.nextDouble()*-100;
+			result = first + second;
+			LOG.info("Testing the method add with " +first+" and "+second);
+			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
+		}
+	}
+	@Test
+	public void test7() {
+		
+		double first = 0.0;
+		double second = 0.0;
+		double result = 0.0;
+		
+		for (int i = 0; i < 5; i++){
+			first = rand1.nextDouble()*-1000;
+			second = rand1.nextDouble()*-1000;
+			result = first + second;
+			LOG.info("Testing the method add with " +first+" and "+second);
+			assertEquals(Math.round(calc.add(first, second)), Math.round(result));
+		}
+	}
 
 }
